09838e0a25a6b7b18798e53aaf3cae0e995168f0
Mert Gunduz <merttgg@gmail.com>
2021-12-13 11:51:09 +0300

    5 Exercises Added To 1

---
+++ 1_Basic_Declarations_And_Expressions/5_Exercise/exercise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+      // Exercise 6
+      // Write a program that calculates the area of square by taking the value from user.
+
+      int squareEdge;
+
+      printf("Enter Square Edge --> ");
+      scanf("%d", &squareEdge);
+
+      printf("\n");
+
+      printf("Square Area --> %d", squareEdge * squareEdge);
+      
+}
