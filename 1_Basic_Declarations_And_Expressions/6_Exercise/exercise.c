a5d236760e65d7bbd79f85404a0542c013a2ea52
Mert Gunduz <merttgg@gmail.com>
2021-12-13 12:01:49 +0300

    6 Exercise Added To 1

---
+++ 1_Basic_Declarations_And_Expressions/6_Exercise/exercise.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main(void)
+{
+      // Exercise 6
+      // Write a C program to compute the perimeter and area of a circle with a given radius.
+
+      int radius;
+
+      printf("Enter Circle Radius --> ");
+      scanf("%d", &radius);
+
+      printf("\n");
+
+      printf("Circle Perimeter --> %f\n", 2 * PI * radius);
+      printf("Circle Area --> %f\n", PI * radius * radius);
+
+}
