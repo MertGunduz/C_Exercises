e80b5deb6573ebe7a938278086f6c8ccc6041e81
Mert Gunduz <merttgg@gmail.com>
2021-12-15 18:46:49 +0300

    15 Added To 1

---
+++ 1_Basic_Declarations_And_Expressions/15_Exercise/exercise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+      // Exercise 15
+      // Write a C program that prints all even numbers between 1 and 50 (inclusive)
+
+      for (int i = 0; i <= 50; i++)
+      {
+            if (i % 2 == 0)
+            {
+                  printf("%d\n", i);
+            }
+      }
+}
